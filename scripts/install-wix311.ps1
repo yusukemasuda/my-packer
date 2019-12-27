f92e85365e5b447487334cadce1e67d390927a4f
Yusuke Masuda <>
2019-12-28 05:21:07 +0900

    refs #20 Separated a portion, that enables .NET Framework 3.5 in install-wix-311.ps1, to a new script file.

--- scripts/install-wix311.ps1
+++ scripts/install-wix311.ps1
@@ -1,27 +1,5 @@
 ﻿
-function EnsureNetFramework3Installed
-{
-  $RegistryKey = "Registry::HKLM\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5"
-  if ((!(Test-Path -Path $RegistryKey)) -Or ((Get-Item -Path $RegistryKey).GetValue("Install") -ne 1))
-  {
-      Write-Host "Enabling .NET Framework 3.1..." -ForegroundColor Cyan
-      $Arguments = @("/c", "DISM.exe /Quiet /Online /Enable-Feature /FeatureName:NetFX3 /All")
-      $process = Start-Process -FilePath "cmd.exe" -ArgumentList $Arguments -Wait -PassThru
-      $exitCode = $process.ExitCode
-      if ($exitCode -eq 0 -or $exitCode -eq 3010)
-      {
-          Write-Host ".NET Framework 3.1 enabled." -ForegroundColor Cyan
-          return $exitCode
-      }
-      else
-      {
-          Write-Host -Object "Non zero exit code returned by the installation process : $exitCode."
-          # this wont work because of log size limitation in extension manager
-          # Get-Content $customLogFilePath | Write-Host
-          exit $exitCode
-      }
-  }
-}
+$InstallerURL = "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe"
 
 function InstallWixToolset
 {
@@ -45,10 +23,6 @@ function InstallWixToolset
 }
 
 #-------------------------------------------------------------------------------
-
-$InstallerURL = "https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe"
-
-EnsureNetFramework3Installed
 
 Write-Host "Installing WiX 3.11.2 ..." -ForegroundColor Cyan
 InstallWixToolset -InstallerURL $InstallerURL
