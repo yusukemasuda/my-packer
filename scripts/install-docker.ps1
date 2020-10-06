7ff377c0accf5887d40eb75f944c46a55219d3cf
Yusuke Masuda <>
2020-10-06 09:11:28 +0900

    Created a script that installs Docker Desktop for WIndows.

---
+++ scripts/install-docker.ps1
@@ -0,0 +1,34 @@
+﻿function InstallDockerWindows() {
+   $DockerURL = "https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe"
+   $exitCode = -1
+   $FilePath = "${env:Temp}\Docker Desktop Installer.exe"
+
+   Write-Host "Downloading Docker Desktop for Windows ..."
+   if (Test-Path -PathType Leaf -Path "$FilePath")
+   {
+       Remove-Item -Path "$FilePath"
+   }
+   Invoke-WebRequest -Uri "$DockerURL" -OutFile "$FilePath"
+   $process = Start-Process -FilePath "`"$FilePath`"" -ArgumentList ("install", "--quiet") -Wait -PassThru
+   $exitCode = $process.ExitCode
+   if ($exitCode -eq 0)
+   {
+       Write-Host "Installed Docker Desktop for Windows successfully." -ForegroundColor Cyan
+       return $exitCode
+   }
+   else
+   {
+       Write-Host -Object "Non zero exit code returned by the installation process : $exitCode."
+       # this wont work because of log size limitation in extension manager
+       # Get-Content $customLogFilePath | Write-Host
+       exit $exitCode
+   }
+}
+
+#-------------------------------------------------------------------------------
+$ErrorActionPreference = "Stop"
+
+$exitCode = InstallDockerWindows
+
+Write-Host -Object "Installation successful"
+exit $exitCode
